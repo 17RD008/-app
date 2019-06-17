6417b1a4cc48f92637e6758772effcc66d73c5b9
17RD008 <17rd008@dendai.ms.ac.jp>
2019-06-17 16:49:54 +0900

    Initial Commit

---
+++ メモ保存app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  メモ保存app
+//
+//  Created by 飯ケ谷　好亮 on 2019/06/17.
+//  Copyright © 2019 008. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
